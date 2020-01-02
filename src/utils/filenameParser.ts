99a86edefc5fc48a4e0bdc78dba45b4e3fb71bd4
Robin <1105080+openscript@users.noreply.github.com>
2020-01-02 16:38:57 +0100

    Fix file name parsing

--- src/utils/filenameParser.ts
+++ src/utils/filenameParser.ts
@@ -1,6 +1,6 @@
 import { Language } from '../models/language';
 
-const FILENAME_REGEX = /\.(?<language>[a-z]{2})?\-?(?<region>[A-Z]{2})?\./gim;
+const FILENAME_REGEX = /(?<=\.)(?<language>[a-z]{2})(\-(?<region>[A-Z]{2}))?(?=\.)/i;
 
 export const parseFilename = (filename: string): Language => {
   const result = FILENAME_REGEX.exec(filename);
